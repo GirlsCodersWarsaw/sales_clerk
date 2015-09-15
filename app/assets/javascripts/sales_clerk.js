8980ba75ffbdc3a7989c4e111e105dc99f52954e
Basia Klosowska <barbaraklosowska@gmail.com>
2015-09-15 15:25:02 +0200

    show view according to payment method and status rubyclerks/sales_clerk#7

--- app/assets/javascripts/sales_clerk.js
+++ app/assets/javascripts/sales_clerk.js
@@ -37,4 +37,12 @@ $(document).ready(function(){
 $(document).ready(function(){
   $(".alert-shop").delay(5000).fadeOut(400);
 });
-  
+
+// show transfer data after click "money transfer" button on order page:
+
+$(document).ready(function() {
+  $('.custom-button').click(function() {
+    var detailsDiv = $('#payment-data');
+    detailsDiv.css("display", "block");
+  });
+});
